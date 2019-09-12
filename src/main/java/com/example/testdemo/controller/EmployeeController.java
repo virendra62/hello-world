dab13caf992ef952771c0475dc33878f33bcc8aa
virendra <rajvirendra1989@gmail.com>
2019-09-12 18:06:46 +0530

    Update

--- src/main/java/com/example/testdemo/controller/EmployeeController.java
+++ src/main/java/com/example/testdemo/controller/EmployeeController.java
@@ -27,13 +27,6 @@ public class EmployeeController {
         return list;
     }
 
-/*    @RequestMapping("/openEmployeeView")
-    public ModelAndView openEmployeeAddView() {
-        ModelAndView mav = new ModelAndView("employeesAdd");
-        mav.addObject("employee", new Employee());
-        return mav;
-    }*/
-
     @PostMapping("/save")
     public ResponseEntity  save(@ModelAttribute("employee") Employee employeeObj) {
         ModelAndView mav = new ModelAndView("employeesList");
